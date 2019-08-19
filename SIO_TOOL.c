bca4d5582118963d188abc7b174290ba139d4ed5
kevin_lee <kevin_lee@lannerinc.com>
2019-08-19 16:05:16 +0800

    first commit dump register value for super io f81216

---
+++ SIO_TOOL.c
@@ -0,0 +1,109 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/io.h>
+#include <sys/ioctl.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <math.h>
+#include <dlfcn.h>
+
+#define int8_t  		char
+#define int16_t 		short
+#define int32_t 		int
+#define int64_t  		long long
+#define uint8_t  		unsigned char
+#define uint16_t 		unsigned short
+#define uint32_t 		unsigned int
+#define uint64_t 		unsigned long long
+
+
+#define SIO_CFGINDEX	0x2E
+#define SIO_CFGDATA	0x2F
+
+void __sio_unlock(void);
+void __sio_lock(void);
+void __sio_logic_device(char num);
+
+
+
+
+
+
+/********************************************************************/
+/***** SuperIO access functions *************************************/
+void __sio_unlock(void)
+{
+	ioperm(SIO_CFGINDEX, 2, 1);
+	outb(0x77 , SIO_CFGINDEX);
+	outb(0x77 , SIO_CFGINDEX);
+}
+/***********/
+void __sio_lock(void)
+{
+	outb(0xaa , SIO_CFGINDEX);
+	ioperm(SIO_CFGINDEX, 2, 0);
+}
+/***********/
+void __sio_logic_device(char num)
+{
+	outb(0x7 , SIO_CFGINDEX);
+	outb(num , SIO_CFGDATA);
+}
+/************/
+uint8_t read_sio_reg(uint8_t LDN, uint8_t reg)
+{
+        outb(0x07, SIO_CFGINDEX); //LDN register
+        outb(LDN, SIO_CFGDATA);
+        outb(reg , SIO_CFGINDEX);
+        return inb(SIO_CFGDATA);
+}
+/************/
+uint8_t write_sio_reg(uint8_t LDN, uint8_t reg, uint8_t value)
+{	
+        outb(0x07, SIO_CFGINDEX); //LDN register
+        outb(LDN, SIO_CFGDATA);
+        outb(reg, SIO_CFGINDEX);
+        outb(value, SIO_CFGDATA);
+        return 0;
+}
+/****************/
+int main( int argc, char** argv )
+{
+uint16_t wAddr =0, xData=0;
+	
+	__sio_unlock();
+	__sio_logic_device(0x00);
+outb(0x70, SIO_CFGINDEX);
+outb(0x04, SIO_CFGDATA);
+
+outb(0x25, SIO_CFGINDEX);
+outb(0x01, SIO_CFGDATA);
+for(int xi=0;xi<=255;xi++)
+{
+	outb(xi, SIO_CFGINDEX);
+	xData=(uint16_t)inb(SIO_CFGDATA);
+printf("0x%X = 0x%X\n",xi,xData);
+}
+	outb(0x23, SIO_CFGINDEX);
+	xData=(uint16_t)inb(SIO_CFGDATA);
+//printf("vendor id=%X\n",xData);
+	outb(0x25, SIO_CFGINDEX);
+	xData=(uint16_t)inb(SIO_CFGDATA);
+//printf("clock select=%X\n",xData);
+
+
+//	outb(0x60, SIO_CFGINDEX);
+//	xData=(uint16_t)inb(SIO_CFGDATA);
+//	outb(0x61, SIO_CFGINDEX);
+//	wAddr=(uint16_t)inb(SIO_CFGDATA);
+//	wAddr= wAddr | (xData << 8 ) ;
+	__sio_lock();
+	
+	return 0;
+	
+}
+
